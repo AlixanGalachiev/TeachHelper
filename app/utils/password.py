de97121178dbfc3902337b77ee2c167ce19aecd0
AlixanGalachiev <all.gall1@mail.ru>
2025-10-23 13:02:12 +0300

    updated python to 3.18.8
    updated bcrypt to 5.0.0
    
    appended httpx pytest-dotenv

--- app/utils/password.py
+++ app/utils/password.py
@@ -1,10 +1,10 @@
 from passlib.context import CryptContext
 
 
-pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+pwd_context = CryptContext(schemes=["Argon2"], deprecated="auto")
 
 def get_password_hash(password):
-	return pwd_context.hash(password)
+    return pwd_context.hash(password)
 
 def verify_password(plain_password, hashed_password):
-	return pwd_context.verify(plain_password, hashed_password)
+    return pwd_context.verify(plain_password, hashed_password)
